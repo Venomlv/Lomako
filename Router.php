cdaa60d38713cc59b2f3e1bbb67884ef95cad378
Denis-PC\Denis <papa236@inbox.lv>
2018-02-28 23:53:52 +0200

    Update 0.0.002

--- Router.php
+++ Router.php
@@ -2,5 +2,20 @@
 
 class Router
 {
+	private $ways;
+	
+	public function __construct()
+	{
+		$this->ways = include(ROOT.'/options/ways.php');
+	}
+	
+	private function getURI()
+	{
+		if(!empty($_SERVER['REQUEST_URI']))
+		{
+			return trim($_SERVER['REQUEST_URI'], '/');
+		}
+	}
+	
 	return true;
 }
